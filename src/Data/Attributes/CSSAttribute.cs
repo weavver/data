52ba5d62d7169d8e2e8eae52cb989a59dcde0897
Mitchel Constantin <mythicalbox@weavver.com>
2014-04-07 10:14:17 -0700

    adding missing css attribute class

---
+++ src/Data/Attributes/CSSAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Weavver.Data
+{
+     [AttributeUsage(AttributeTargets.All, AllowMultiple = true)]
+     public class CSSAttribute : Attribute
+     {
+          public string _CSS;
+
+          public CSSAttribute() { }
+
+          public CSSAttribute(string CSS)
+          {
+               _CSS = CSS;
+          }
+     }
+}
