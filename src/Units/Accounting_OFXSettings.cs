31f8c1ff756c19bf3c6cb112c3f87d5dfe831acb
Mitchel Constantin <mythicalbox@weavver.com>
2014-10-07 18:23:14 -0700

    reverting the breaking change

--- src/Units/Accounting_OFXSettings.cs
+++ src/Units/Accounting_OFXSettings.cs
@@ -8,10 +8,6 @@ using Weavver.Testing;
 
 namespace Weavver.Testing.Accounting
 {
-     
-     testing if this will break the build
-
-
      //[TestFixture]
      public class Accounting_OFXSettingsTest
      {
